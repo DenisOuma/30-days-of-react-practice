31aff5d85d663bf9c8af48e259bbbeb14a410159
DenisOuma <dennisouma38@gmail.com>
2022-12-30 23:40:47 +0300

    first commit

---
+++ 01-day-react/index.js
@@ -0,0 +1,15 @@
+// JSX syntax
+const jsxElement = <h1>I am a JSX element</h1>;
+const welcome = <h1>Welcome to 30 Days of React Challenge</h1>;
+const data = <small>Oct 2, 2020</small>;
+const title = <h2>Getting Started React</h2>
+
+const header = (
+    <header>
+        <h1>Welcome to 30 Days of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>Javascript Library</h3>
+        <p>Denis Ouma</p>
+        <small>Oct 2, 2020</small>
+    </header>
+)
